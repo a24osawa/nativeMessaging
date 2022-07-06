ff9b4df6728588104f74e269c1dbfc6fe079110b
Atsushi Osawa <a24osawa@gmail.com>
2022-07-06 16:18:59 +0900

    added ext-mv3 and host-mv3 for developing MV3 ext

---
+++ ext-mv3/background.js
@@ -0,0 +1,46 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var port = null;
+
+var getKeys = function(obj){
+   var keys = [];
+   for(var key in obj){
+      keys.push(key);
+   }
+   return keys;
+}
+
+
+function appendMessage(text) {
+  document.getElementById('response').innerHTML += "<p>" + text + "</p>";
+}
+
+function sendNativeMessage(text) {
+  message = {"text": text};
+  port.postMessage(message);
+  appendMessage("Sent message: <b>" + JSON.stringify(message) + "</b>");
+}
+
+function onNativeMessage(message) {
+  appendMessage("Received message: <b>" + JSON.stringify(message) + "</b>");
+  sendNativeMessage(message.text);
+}
+
+function onDisconnected() {
+  appendMessage("Failed to connect: " + chrome.runtime.lastError.message);
+  port = null;
+}
+
+function connect() {
+  var hostName = "com.google.chrome.example.echo";
+  appendMessage("Connecting to native messaging host <b>" + hostName + "</b>")
+  port = chrome.runtime.connectNative(hostName);
+  port.onMessage.addListener(onNativeMessage);
+  port.onDisconnect.addListener(onDisconnected);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  connect();
+});
