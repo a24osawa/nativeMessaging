7c74660bf5ebbce353788de66895d991b67853f5
Atsushi Osawa <a24osawa@gmail.com>
2022-07-11 15:34:35 +0900

    added windows.onCreated listener

--- ext-mv3/background.js
+++ ext-mv3/background.js
@@ -4,6 +4,16 @@
 
 var port = null;
 
+chrome.runtime.onInstalled.addListener(({ reason, version }) => {
+  console.log("chrome.runtime.onInstalled");
+  connect();
+});
+
+chrome.windows.onCreated.addListener(({ object}) => {
+  console.log("chrome.windows.onCreated");
+  connect();
+});
+
 function sendNativeMessage(text) {
   console.log("sendNativeMessage()");
   message = {"text": text};
@@ -18,17 +28,19 @@ function onNativeMessage(message) {
 function onDisconnected() {
   console.log("onDisconnected()");
   port = null;
+  connect();
 }
 
 function connect() {
   console.log("connect()");
   var hostName = "com.google.chrome.example.echo.mv3";
-  port = chrome.runtime.connectNative(hostName);
-  port.onMessage.addListener(onNativeMessage);
-  port.onDisconnect.addListener(onDisconnected);
+  if (port != null) {
+    console.log("port is already connected.");
+  }
+  else {
+    console.log("connecting new port...");
+    port = chrome.runtime.connectNative(hostName);
+    port.onMessage.addListener(onNativeMessage);
+    port.onDisconnect.addListener(onDisconnected);  
+  }
 }
-
-chrome.runtime.onInstalled.addListener(({ reason, version }) => {
-  console.log("Event.addListener()");
-  connect();
-});
